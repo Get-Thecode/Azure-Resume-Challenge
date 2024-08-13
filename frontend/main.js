fe0af56e5411175bb7955a177a6f88c2644c111c
Ankur Yadav <yadavankur44@gmail.com>
2024-08-13 21:15:14 +0530

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event)=>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getcounterresume.azurewebsites.net/api/GetResumeCounter?code=R7hke_bRZGxsOoa_nGC9L1c2_s9uUKCkYUnvm4jMcYcxAzFu3G8--A%3D%3D';
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API");
